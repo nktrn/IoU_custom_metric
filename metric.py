4fdb01447eebf44b15363ae406ee588f2993c921
Nikita Aparovich <o.pelmen.o@gmail.com>
2022-05-18 10:42:14 +0300

    initial commit

---
+++ metric.py
@@ -0,0 +1,32 @@
+from itertools import product
+
+from geometry import Polygon, intersection
+
+
+def iou(true_polygon: Polygon, predict_polygon: Polygon) -> float:
+    t_points = [
+        vertex
+        for vertex in true_polygon.vertexes
+        if predict_polygon.contains_point(vertex)
+    ]
+    p_points = [
+        vertex
+        for vertex in predict_polygon.vertexes
+        if true_polygon.contains_point(vertex)
+    ]
+
+    if len(t_points) == 0 and len(p_points) == 0:
+        return 0
+
+    i_points = [
+        intersection(edges[0], edges[1])
+        for edges in product(true_polygon.get_edges(), predict_polygon.get_edges())
+        if intersection(edges[0], edges[1])
+    ]
+
+    points = t_points + p_points + i_points
+
+    intersection_polygon = Polygon(*points)
+    res = intersection_polygon.area / (predict_polygon.area + true_polygon.area - intersection_polygon.area)
+
+    return res
---
+++ tests/test_line.py
@@ -0,0 +1,46 @@
+from geometry import Point, Line
+
+
+def test_contains_point():
+    line = Line(
+        Point(0, 0),
+        Point(100, 0)
+    )
+    point = Point(50, 0)
+    assert line.contains_point(point)
+
+
+def test_contains_point__point_not_in_line():
+    line = Line(
+        Point(0, 0),
+        Point(100, 0)
+    )
+    point = Point(50, 50)
+    assert line.contains_point(point) == False
+
+
+def test_orientation_point_on_the_left():
+    line = Line(
+        Point(0, 100),
+        Point(0, 0)
+    )
+    point = Point(50, 50)
+    assert line.orientation_is_left(point)
+
+
+def test_orientation_point_on_the_right():
+    line = Line(
+        Point(100, 100),
+        Point(0, 0)
+    )
+    point = Point(0, 50)
+    assert line.orientation_is_left(point) == False
+
+
+def test_orientation_point_on_line():
+    line = Line(
+        Point(100, 100),
+        Point(0, 0)
+    )
+    point = Point(50, 50)
+    assert line.orientation_is_left(point)
